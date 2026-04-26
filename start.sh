92a815e9dc629c33675435142a9d8cd68146feaa
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2026-04-26 14:37:30 +0100

    Add VSCodium installation to start.sh
    Added installation steps for VSCodium including repository setup and version check.

--- start.sh
+++ start.sh
@@ -61,6 +61,17 @@ rm obs-studio.deb
 echo "OBS version installed:"
 obs --version
 
+echo "Installing VSCodium..."
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | tee /etc/apt/sources.list.d/vscodium.list
+apt-get update
+apt-get install -y codium
+echo "VSCodium version installed:"
+codium --version
+
 echo "Desktop user already configured..."
 echo "Killing any existing processes..."
 pkill -9 rustdesk || true
